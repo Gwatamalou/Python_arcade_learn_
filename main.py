8a289cc8fcc1cd5844bd192f9dc12900796c22d8
gwatamalou <gwatir@yandex.ru>
2024-06-28 17:57:42 +0600

    Add class w1

--- main.py
+++ main.py
@@ -6,21 +6,31 @@ WINDOW_HEIGHT = 600
 WINDOW_TITLE = 'P_Arcade'
 
 
-class Button():
-    class StartButton(arcade.gui.UIFlatButton):
+class StartButton(arcade.gui.UIFlatButton):
         pass
 
-    class SettingsButton(arcade.gui.UIFlatButton):
-        pass
+class SettingsButton(arcade.gui.UIFlatButton):
+    pass
 
-    class QuitButton(arcade.gui.UIFlatButton):
-        def on_click(self, event: arcade.gui.UIOnClickEvent):
-            arcade.exit()
+class QuitButton(arcade.gui.UIFlatButton):
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        arcade.exit()
 
 
-class MyGame(arcade.Window):
+class Window(arcade.Window):
+    def __init__(self, window_width = WINDOW_WIDTH, window_height = WINDOW_HEIGHT, window_title = WINDOW_TITLE):
+        super().__init__(window_width, window_height, window_title)
+        self.window = arcade.gui.UIManager()
+        self.window.enable()
+
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+
+
+
+class MyGame(Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        self.window = Window()
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
 
@@ -28,13 +38,13 @@ class MyGame(arcade.Window):
 
         self.b_v_box = arcade.gui.UIBoxLayout()
 
-        start_button = Button.StartButton(text='Start', width=200)
+        start_button = StartButton(text='Start', width=200)
         self.b_v_box.add(start_button.with_space_around(bottom=20))
 
-        settings_button = Button.SettingsButton(text='Settings', width=200)
+        settings_button = SettingsButton(text='Settings', width=200)
         self.b_v_box.add(settings_button.with_space_around(bottom=20))
 
-        quit_button = Button.QuitButton(text='Quit', width=200)
+        quit_button = QuitButton(text='Quit', width=200)
         self.b_v_box.add(quit_button)
 
         self.manager.add(
