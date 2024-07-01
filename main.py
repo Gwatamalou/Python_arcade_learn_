d790ece531de9d3c1e26e1f3636ab925a8e6ea54
gwatamalou <gwatir@yandex.ru>
2024-07-01 11:58:14 +0600

    Add class menu, add go to menu window,button event tracking moved to decorators

--- main.py
+++ main.py
@@ -6,46 +6,36 @@ WINDOW_HEIGHT = 600
 WINDOW_TITLE = 'P_Arcade'
 
 
-class StartButton(arcade.gui.UIFlatButton):
-        pass
+class MainView(arcade.View):
+    """окно начального экрана"""
 
-class SettingsButton(arcade.gui.UIFlatButton):
-    pass
-
-class QuitButton(arcade.gui.UIFlatButton):
-    def on_click(self, event: arcade.gui.UIOnClickEvent):
-        arcade.exit()
-
-
-class Window(arcade.Window):
-    def __init__(self, window_width = WINDOW_WIDTH, window_height = WINDOW_HEIGHT, window_title = WINDOW_TITLE):
-        super().__init__(window_width, window_height, window_title)
-        self.window = arcade.gui.UIManager()
-        self.window.enable()
-
-        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
-
-
-
-class MyGame(Window):
     def __init__(self):
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-        self.window = Window()
+        super().__init__()
         self.manager = arcade.gui.UIManager()
-        self.manager.enable()
-
-        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
 
         self.b_v_box = arcade.gui.UIBoxLayout()
 
-        start_button = StartButton(text='Start', width=200)
+        start_button = arcade.gui.UIFlatButton(text="Start", width=200)
         self.b_v_box.add(start_button.with_space_around(bottom=20))
 
-        settings_button = SettingsButton(text='Settings', width=200)
+        settings_button = arcade.gui.UIFlatButton(text="Settings", width=200)
         self.b_v_box.add(settings_button.with_space_around(bottom=20))
 
-        quit_button = QuitButton(text='Quit', width=200)
+        quit_button = arcade.gui.UIFlatButton(text="Quit", width=200)
         self.b_v_box.add(quit_button)
+
+        @start_button.event('on_click')
+        def on_click_start_button(event):
+            pass
+
+        @settings_button.event('on_click')
+        def on_click_settings_button(event):
+            main_view = MenuView(self)
+            self.window.show_view(main_view)
+
+        @quit_button.event('on_click')
+        def on_click_quit_button(event):
+            arcade.exit()
 
         self.manager.add(
             arcade.gui.UIAnchorWidget(
@@ -53,13 +43,68 @@ class MyGame(Window):
                 anchor_y="center_y",
                 child=self.b_v_box))
 
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+        self.manager.enable()
+
+    def on_hide_view(self):
+        self.manager.disable()
+
+    def on_draw(self):
+        self.clear()
+
+        self.manager.draw()
+
+
+class MenuView(arcade.View):
+    """окно меню"""
+
+    def __init__(self, main_view):
+        super().__init__()
+
+        self.manager = arcade.gui.UIManager()
+
+        self.b_v_box = arcade.gui.UIBoxLayout()
+
+        back_button = arcade.gui.UIFlatButton(text="Back", width=200)
+        self.b_v_box.add(back_button)
+
+        @back_button.event('on_click')
+        def on_click_quit_button(event):
+            self.window.show_view(main_view)
+
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="center_y",
+                child=self.b_v_box))
+
+        self.main_view = main_view
+
+    def on_hide_view(self):
+        self.manager.disable()
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.DARK_BLUE_GRAY)
+
+        self.manager.enable()
+
     def on_draw(self):
         self.clear()
         self.manager.draw()
 
 
+class MyGame(arcade.Window):
+
+    # создание окна
+    def __init__(self, window_width=WINDOW_WIDTH, window_height=WINDOW_HEIGHT, window_title=WINDOW_TITLE):
+        super().__init__(window_width, window_height, window_title)
+
+
 def main():
     window = MyGame()
+    main_view = MainView()
+    window.show_view(main_view)
     arcade.run()
 
 
